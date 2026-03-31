7686c2f472f0ae13aad708777ab9ecb042c5abbb
Bhaurao <bhauraomane@gmail.com>
2026-03-31 17:51:49 +0530

    updated test.py file

--- tests.py
+++ tests.py
@@ -1,15 +1,15 @@
-# Write a python program to perform unit testing on the sum_of_two_numbers function.
-import unittest
+# Write python program to perform unit testing on the sum_of_two_numbers function and print the results if test passes or fails.
+import pytest
 from main import sum_of_two_numbers
 
-class TestSumOfTwoNumbers(unittest.TestCase):
-    def test_sum_of_two_numbers(self):
-        self.assertEqual(sum_of_two_numbers(10, 20), 30)
-        self.assertEqual(sum_of_two_numbers(-5, 5), 0)
-        self.assertEqual(sum_of_two_numbers(0, 0), 0)
-        self.assertEqual(sum_of_two_numbers(-10, -20), -30)
+def test_sum_of_two_numbers():
+    assert sum_of_two_numbers(10, 20) == 30
+    assert sum_of_two_numbers(-5, 5) == 0
+    assert sum_of_two_numbers(0, 0) == 0
+    assert sum_of_two_numbers(-3, -7) == -10
+if __name__ == "__main__":
+    pytest.main()
 
-if __name__ == '__main__':
-    unittest.main()
+
 
     
